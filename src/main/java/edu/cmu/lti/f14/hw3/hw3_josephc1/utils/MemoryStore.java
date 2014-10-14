31e310e079c95531e0df8ee78b7951df573d66b0
josephcc <josephcc@cmu.edu>
2014-10-14 02:01:34 -0400

    Singleton memory store

---
+++ src/main/java/edu/cmu/lti/f14/hw3/hw3_josephc1/utils/MemoryStore.java
@@ -0,0 +1,37 @@
+package edu.cmu.lti.f14.hw3.hw3_josephc1.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryStore {
+  
+  private static Map<String, MemoryStore> singletonInstances;
+  public HashMap<String, Object> data;
+
+  // SingletonExample prevents any other class from instantiating
+  /**
+   * Initializer
+   * 
+   * 
+   */
+  private MemoryStore() {
+    data = new HashMap<String, Object>();
+  }
+
+  /**
+   * Global point of entry to the singleton classes with unique models
+   * 
+   * @param tableName
+   * @return
+   */
+  public static MemoryStore getSingletonInstance(String tableName) {
+    if (null == singletonInstances) {
+      singletonInstances = new HashMap<String, MemoryStore>();
+    }
+    if (!singletonInstances.containsKey(tableName)) {
+      singletonInstances.put(tableName, new MemoryStore());
+    }
+    return singletonInstances.get(tableName);
+  }
+
+}
